7e4a11be793df288e4b7955968d658c284fc4d6a
david.helmy <david.helmy@brightskiesinc.com>
2023-07-16 13:14:50 +0300

    benchmark minor edit

--- benchmarks/BenchmarkMMPR/solve.R
+++ benchmarks/BenchmarkMMPR/solve.R
@@ -1,11 +1,45 @@
 library(rbenchmark)
 library(MMPR)
 
+
+
+generate_matrix_big <- function(n, m) {
+  # Set the matrix dimensions
+  nrows <- n
+  ncols <- m
+
+  # Set the number of submatrices
+  n_submatrices <- 4
+
+  # Determine the number of rows and columns for each submatrix
+  sub_nrows <- ceiling(nrows / sqrt(n_submatrices))
+  sub_ncols <- ceiling(ncols / sqrt(n_submatrices))
+
+  # Generate random values for each submatrix
+  sub_matrices <- lapply(1:n_submatrices, function(x) {
+    rand_vals <- rnorm(sub_nrows * sub_ncols)
+    matrix(rand_vals, nrow = sub_nrows, ncol = sub_ncols)
+  })
+
+  # Combine the submatrices into a single matrix
+  my_matrix <- do.call(cbind, lapply(split(sub_matrices, rep(1:2, each = 2)), function(x) {
+    do.call(rbind, x)
+  }))
+
+  return(my_matrix)
+}
+
+
 run_solve_benchmark <- function(n, replication, times) {
 
   # Create a random matrix
   set.seed(123)
-  A <- matrix(rnorm(n * n), nrow = n, ncol = n)
+  if (n > 20000) {
+    A <- generate_matrix_big(n, n)
+  }
+  else {
+    A <- matrix(rnorm(n^2), ncol = n)
+  }
 
   # Create a random vector
   b <- rnorm(n)
