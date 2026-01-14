6c2dbf52ac36be31df46e21d2a3a6608fef5e3d6
Ali Hakam <ali.hakam@brightskiesinc.com>
2026-01-14 15:26:02 +0200

    fixed corssproduct output matrix defaulted to CPU even for half precision

--- src/adapters/RLinearAlgebra.cpp
+++ src/adapters/RLinearAlgebra.cpp
@@ -9,6 +9,7 @@
 #include <adapters/RLinearAlgebra.hpp>
 #include <adapters/RHelpers.hpp>
 #include <kernels/Promoter.hpp>
+#include <kernels/ContextManager.hpp>
 #include <utilities/MPCRDispatcher.hpp>
 
 
@@ -133,7 +134,11 @@ RCrossProduct(DataType *aInputA, SEXP aInputB) {
 
     auto precision = aInputA->GetPrecision();
 
-    auto pOutput = new DataType(precision);
+    // Get current operation placement to allocate output correctly
+    auto context = ContextManager::GetOperationContext();
+    auto operation_placement = context->GetOperationPlacement();
+
+    auto pOutput = new DataType(precision, operation_placement);
     SIMPLE_DISPATCH_WITH_HALF(precision, linear::CrossProduct, *aInputA,
                               *temp_b, *pOutput, transpose, false)
 
@@ -179,7 +184,11 @@ RTCrossProduct(DataType *aInputA, SEXP aInputB) {
 
     auto precision = aInputA->GetPrecision();
 
-    auto pOutput = new DataType(precision);
+    // Get current operation placement to allocate output correctly
+    auto context = ContextManager::GetOperationContext();
+    auto operation_placement = context->GetOperationPlacement();
+
+    auto pOutput = new DataType(precision, operation_placement);
     SIMPLE_DISPATCH_WITH_HALF(precision, linear::CrossProduct, *aInputA, *temp_b,
                     *pOutput,
                     false, true)
